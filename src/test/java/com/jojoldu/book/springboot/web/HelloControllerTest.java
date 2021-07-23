d68a69426d7bdb7cd5c7f3e331146bde55975e5e
hasunzo <gktjsdl470@gmail.com>
2021-07-23 17:26:27 +0900

    Chapter02 스프링부트 테스트코드와 롬복

---
+++ src/test/java/com/jojoldu/book/springboot/web/HelloControllerTest.java
@@ -0,0 +1,44 @@
+package com.jojoldu.book.springboot.web;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.hamcrest.Matchers.is;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+@RunWith(SpringRunner.class) // 스프링 부트 테스트와 Junit 사이에 연결자 역할
+@WebMvcTest // Web에 집중할 수 있는 어노테이션 @Controller, @ControllerAdvice 등을 사용할 수 있으나
+            // @Service, @Component, @Repository 등은 사용할 수 없다
+public class HelloControllerTest {
+
+    @Autowired
+    private MockMvc mvc;
+
+    @Test
+    public void hello가_리턴된다() throws Exception {
+        String hello = "hello";
+
+        mvc.perform(get("/hello"))
+                .andExpect(status().isOk())
+                .andExpect(content().string(hello));
+    }
+
+    @Test
+    public void helloDto가_리턴된다() throws Exception {
+        String name = "hello";
+        int amount = 1000;
+
+        mvc.perform(
+                    get("/hello/dto")
+                            .param("name", name)
+                            .param("amount", String.valueOf(amount)))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.name", is(name)))
+                .andExpect(jsonPath("$.amount", is(amount)));
+    }
+}
